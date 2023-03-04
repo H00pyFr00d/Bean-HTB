18b34daebcd113a32ca0ed1cf4ccc2549927cbb0
H00pyFr00d <kiff2002@gmail.com>
2023-03-04 14:56:20 +0000

    Added react basics

---
+++ htb-bean/src/components/Footer.js
@@ -0,0 +1,10 @@
+import '../App.css'; 
+import logo from '../images/BEANLGOO.png';
+
+export default function Navbar() {
+    return (
+        <div>
+            <img className="logo" src={logo} alt="Logo"></img>
+        </div>
+    )
+}
