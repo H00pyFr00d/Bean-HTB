fbe0f6edd4f7fd92a38a7cd7616581466b0a7f94
H00pyFr00d <kiff2002@gmail.com>
2023-03-04 20:12:13 +0000

    Find a bin fixed

--- htb-bean/src/App.js
+++ htb-bean/src/App.js
@@ -8,8 +8,9 @@ import binLogo from "./images/binLogo.png";
 
 
 function App() {
-  const [homePage, setHomePage] = useState(true)
+  const [homePage, setHomePage] = useState(true);
   const [mapPage, setMapPage] = useState(false);
+  const [filterPage, setFilterPage] = useState(false);
 
   const [coords, setCoords] = useState(null);
   const [map, setMap] = useState("https://www.openstreetmap.org/export/embed.html?bbox=-3.2060337066650395%2C55.93855329531538%2C-3.1777095794677734%2C55.94984861420047&amp;layer=mapnik");
@@ -66,12 +67,21 @@ function App() {
   const goToHome = () => {
     setHomePage(true);
     setMapPage(false);
+    setFilterPage(false);
+  }
+
+  const goToFilter = () => {
+    setHomePage(false);
+    setMapPage(false);
+    setFilterPage(true);
   }
 
   const goToMap = () => {
     setHomePage(false);
     setMapPage(true);
+    setFilterPage(false);
   }
+
  
   const Checkbox = ({ label }) => {
     const [isChecked, setIsChecked] = useState(false);
@@ -104,14 +114,61 @@ function App() {
                           <div onClick={goToHome} class="navbar_links">Home</div>
                       </li>
                       <li className="navbar_item">
-                          <div onClick={goToMap} class="navbar_links">Find a Bin</div>
+                          <div onClick={goToFilter} class="navbar_links">Find a Bin</div>
                       </li>
                     </ul>
             </div>
             </nav>
         </div>
     )
-}
+  }
+
+  const Filters = () => {
+    return (
+      <div className='filterMainBody'>
+        <h2>Please select the campus you are closest to:</h2>
+
+        <div className = "areaButtons">
+          <input type="checkbox" id="areaButton" name="central" value="Central"/>
+          <label for="central"> Central Campus</label>
+          <input type="checkbox" id="areaButton" name="kings" value="Kings"/>
+          <label for="kings"> Kings Campus</label>
+        </div>
+      
+
+        <div>
+          <h2>Please select which type of rubbish you want to dispose of:</h2>
+
+          <div className = "rubbishButtons">
+              <ul className="rubbish_menu">
+                  <li className="rubbish_item">
+                      <p className="rubbish_links">General Waste</p>
+                  </li>
+                  <li className="rubbish_item">
+                      <p className="rubbish_links">Food Waste</p>
+                  </li>
+                  <li className="rubbish_item">
+                      <p className="rubbish_links">Textile Recycling</p>
+                  </li>
+                  <li className="rubbish_item">
+                      <p className="rubbish_links">Paper Recycling</p>
+                  </li>
+                  <li className="rubbish_item">
+                      <p className="rubbish_links">Bottle Recycling</p>
+                  </li>
+                  <li className="rubbish_item">
+                      <p className="rubbish_links">Packaging Recycling</p>
+                  </li>
+                  <li className="rubbish_item">
+                      <p className="rubbish_links">Book Bank</p>
+                  </li>
+              </ul>
+          </div>
+
+        </div>
+      </div>
+    )
+  }
 
   return (
     <div>
@@ -119,14 +176,7 @@ function App() {
         {Navbar()}
       </header>
 
-      <checkBoxes className = "Check Boxes">
-      {Checkbox("Central Campus") }
-      </checkBoxes>
-      
-      <checkBoxes className = "Check Boxes">
-      {Checkbox("Kings Buildings") }
-      </checkBoxes>
-      
+
 
       {homePage && (
         <div style={{'width': '100%', 'overflow': 'hidden'}}>
@@ -134,25 +184,19 @@ function App() {
         </div>
       )}
 
+      {filterPage && (
+        <div style={{'width': '100%', 'overflow': 'hidden'}}>
+          <Filters/>
+        </div>
+      )}
+
       {mapPage && (
         <div style={{'width': '100%', 'overflow': 'hidden'}}>
 
-          <div style={{"width": "50%", 'height': '84.5vh', 'float': 'left'}}>
+          <div style={{"width": "100vw", 'height': '84.5vh', 'float': 'left'}}>
             {drawMap(map)}
           </div>
-
-          <div style={{"marginLeft": "50%", 'margin': '2%'}}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lobortis in neque eu blandit. Phasellus non dui ac ex pellentesque rhoncus. Pellentesque leo est, venenatis at odio a, pellentesque pharetra sapien. Morbi libero arcu, rhoncus vehicula eros et, elementum malesuada enim. Etiam mollis diam sed iaculis mollis. Proin ornare sem nec elementum vehicula. Phasellus vel scelerisque turpis, eu finibus odio. Cras sit amet nunc id sem maximus efficitur sit amet sit amet arcu. Proin in pharetra risus. Mauris vitae arcu vel urna dapibus pretium sit amet ac mauris.<br/>
-
-            Fusce sit amet mauris tellus. Quisque lobortis mi non purus tristique, sit amet tempus odio semper. Aenean enim tellus, malesuada at lobortis eget, aliquam sit amet diam. Sed interdum consectetur turpis eu suscipit. Duis consequat, sapien eu luctus porta, ligula diam mattis tortor, varius dapibus dui justo eu mauris. Donec eget tempor justo, nec hendrerit ligula. Fusce id interdum magna.<br/>
-
-            Morbi dapibus odio a velit ultrices, ut placerat elit consectetur. Nulla mattis urna ut mollis sodales. Aenean laoreet quis enim sed ornare. Vestibulum quis ante enim. Duis sagittis tincidunt augue, ut egestas metus. Pellentesque semper enim erat, in iaculis risus tristique in. Sed bibendum pretium velit, ut iaculis est pellentesque quis. Sed rhoncus lacus sed dui tristique, ac hendrerit leo vestibulum. Pellentesque bibendum rutrum sapien, eu tincidunt purus. Sed accumsan, orci non feugiat vestibulum, ante lacus semper augue, tincidunt viverra elit felis ut tellus. Suspendisse potenti. Fusce a nibh sit amet arcu imperdiet placerat nec id turpis. Maecenas a justo eget felis suscipit porttitor.<br/>
-
-            Phasellus tempor turpis eu viverra rhoncus. Sed ornare ipsum non luctus ultricies. Ut a sem in ipsum cursus blandit. Praesent vestibulum malesuada vulputate. Curabitur imperdiet enim ex, id venenatis nibh posuere non. Nulla condimentum viverra malesuada. Vivamus ac nisi scelerisque, consequat ante id, hendrerit quam. Suspendisse mi libero, bibendum malesuada vestibulum at, imperdiet ut lectus. Nulla facilisi. Curabitur blandit lorem in risus elementum eleifend. Mauris in massa mi. Donec magna justo, eleifend efficitur tempor ac, congue ut mi. Quisque fringilla leo a lectus aliquet, in eleifend orci sodales. Sed vulputate sem nec arcu pharetra, vitae tristique urna porta.<br/>
-
-            Nunc interdum lacus nec urna suscipit sodales. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent ultricies sit amet nibh eget gravida. Sed non sapien id mauris laoreet placerat elementum eget felis. Integer vel luctus neque. Vestibulum sed nulla ut est ultrices egestas non sed magna. Maecenas a varius dui. Nullam consectetur, erat ut maximus finibus, diam leo fermentum elit, quis aliquam metus leo id quam. Fusce vitae elit convallis, rhoncus nisi nec, semper nisi.<br/>
-            <button onClick={getLocation()}>Reset Coordinates</button>
-          </div>
+          
         </div>
       )}
 
