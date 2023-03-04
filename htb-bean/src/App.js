1001f23d0d98a9a4681730ce09e1fecfc744f368
H00pyFr00d <kiff2002@gmail.com>
2023-03-04 21:41:06 +0000

    Removed three bar menu

--- htb-bean/src/App.js
+++ htb-bean/src/App.js
@@ -242,12 +242,4 @@ function App() {
 
 export default App;
 
-//menu
- const menu = document.querySelector('#mobile-menu');
- const menuLinks = document.querySelector('.navbar_menu');
 
- menu.addEventListener('click', function()
- {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
- });
