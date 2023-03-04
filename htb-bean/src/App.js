ffb777957a380cbeff564707084669ba7ba20ed2
imnotkate <k.rankin07@outlook.com>
2023-03-04 21:26:00 +0000

    fhdjsakl

--- htb-bean/src/App.js
+++ htb-bean/src/App.js
@@ -115,11 +115,7 @@ function App() {
       </div>
     );
   };
-  //export default Checkbox;
-  const Footer = () => {
-    return ( <p> Data sourced from <a href = "https://data.edinburghcouncilmaps.info/datasets/ddb5fcb791634729b4b4d3d1e5b8aa05/explore"> Edinburgh City Council </a> , May 2021. </p>
-    )
-  }
+  
 
   const Navbar = () => {
     return (
--- htb-bean/src/Home.js
+++ htb-bean/src/Home.js
@@ -14,7 +14,6 @@ export default function Home() {
                 web page to show everyone what local recycling facilities are available to them. This web page will show the closest recycling bins to their 
                 current location depending on what they intend to recycle. 
             </p>
-
         </div>
         
     )
