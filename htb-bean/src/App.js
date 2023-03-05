48ddfd7f45fbc55530393960064f536dd663b53a
loveLace0 <88147475+loveLace0@users.noreply.github.com>
2023-03-05 11:21:47 +0000

    Update Local

--- htb-bean/src/App.js
+++ htb-bean/src/App.js
@@ -243,7 +243,6 @@ const drawMap = () => {
               <img src = {title} style={{'marginLeft': '5%'}} alt = "Find a Bin" height = "100%" width = "height" />
               <img src = {binLogo} alt = "Web Logo" height = "100%" width = "height"/>
 
-
               <div className="navbar_container">
                 <div className="navbar_menu">
                   <ul>
